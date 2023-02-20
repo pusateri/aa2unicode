40ff9b289ec57c0a6a885758ae4c8537241e94d2
Tom Pusateri <pusateri@bangj.com>
2023-02-20 18:56:00 -0500

    clean up warnings and bump version

--- src/aa2unicode/main.py
+++ src/aa2unicode/main.py
@@ -7,90 +7,91 @@ from copy import deepcopy
 help = """Module docstring.
 
 Convert artwork from ASCII to Unicode box characters
-lineart [-h | --help]
-lineart <input file>
+aa2unicode [-h | --help]
+aa2unicode <input file>
 
 """
 
+
 def print_chars(chars):
-    for l in range(len(chars)):
-        for c in chars[l]:
+    for achar in range(len(chars)):
+        for c in chars[achar]:
             print(c, end="")
         print("")
 
 
-def above_char(l, c, chars):
-    if l == 0:
+def above_char(line, c, chars):
+    if line == 0:
         return None
-    maxc = len(chars[l-1])
+    maxc = len(chars[line-1])
     if c > maxc - 1:
         return None
-    return chars[l-1][c]
+    return chars[line-1][c]
 
 
-def below_char(l, c, chars):
+def below_char(line, c, chars):
     maxl = len(chars)
-    if l == maxl - 1:
+    if line == maxl - 1:
         return None
-    maxc = len(chars[l+1])
+    maxc = len(chars[line+1])
     if c > maxc - 1:
         return None
-    return chars[l+1][c]
+    return chars[line+1][c]
 
 
-def previous_char(l, c, chars):
+def previous_char(line, c, chars):
     if c == 0:
         return None
-    return chars[l][c-1]
+    return chars[line][c-1]
 
 
-def next_char(l, c, chars):
-    maxc = len(chars[l])
+def next_char(line, c, chars):
+    maxc = len(chars[line])
     if c == maxc - 1:
         return None
-    return chars[l][c+1]
+    return chars[line][c+1]
 
 
-def transform(l, c, old, new):
+def transform(line, c, old, new):
     """Replace select characters in new matrix based on surrounding characters"""
-    if old[l][c] == ' ':
+    if old[line][c] == ' ':
         return
-    elif old[l][c] == '+':
+    elif old[line][c] == '+':
         # left edge
-        p = previous_char(l, c, old)
-        n = next_char(l, c, old)
-        b = below_char(l, c, old)
-        a = above_char(l, c, old)
+        p = previous_char(line, c, old)
+        n = next_char(line, c, old)
+        b = below_char(line, c, old)
+        a = above_char(line, c, old)
         # leading edge
-        if (p == ' ' or p == None) and (n == '-' or n == '+'):
-            if (a == ' ' or a == None or a == '.') and b == '|':
-                new[l][c] = '┌'
+        if (p == ' ' or p is None) and (n == '-' or n == '+'):
+            if (a == ' ' or a is None or a == '.') and b == '|':
+                new[line][c] = '┌'
             elif a == '|' and b == '|':
-                new[l][c] = '├'
-            elif a == '|' and (b == ' ' or b == None or b == '.'):
-                new[l][c] = '└'
+                new[line][c] = '├'
+            elif a == '|' and (b == ' ' or b is None or b == '.'):
+                new[line][c] = '└'
         # interior
         elif (p == '-' or p == '+') and (n == '-' or n == '+'):
             if a != '|' and b != '|':
-                new[l][c] = '─'
+                new[line][c] = '─'
             if a != '|' and b == '|':
-                new[l][c] = '┬'
+                new[line][c] = '┬'
             if a == '|' and b != '|':
-                new[l][c] = '┴'
+                new[line][c] = '┴'
             if a == '|' and b == '|':
-                new[l][c] = '┼'
+                new[line][c] = '┼'
         # trailing edge
-        elif p == '-' and (n == ' ' or n == None):
-            if (a == ' ' or a == None or a == '.') and b == '|':
-                new[l][c] = '┐'
+        elif p == '-' and (n == ' ' or n is None):
+            if (a == ' ' or a is None or a == '.') and b == '|':
+                new[line][c] = '┐'
             if a == '|' and b == '|':
-                new[l][c] = '┤'
-            if a == '|' and (b == ' ' or b == None or b == '.'):
-                new[l][c] = '┘'
-    elif old[l][c] == '-':
-        new[l][c] = '─'
-    elif old[l][c] == '|':
-        new[l][c] = '│'
+                new[line][c] = '┤'
+            if a == '|' and (b == ' ' or b is None or b == '.'):
+                new[line][c] = '┘'
+    elif old[line][c] == '-':
+        new[line][c] = '─'
+    elif old[line][c] == '|':
+        new[line][c] = '│'
 
 
 def main():
@@ -115,12 +116,12 @@ def main():
 
             out = deepcopy(chars)
 
-            for l in range(len(chars)):
-                for c in range(len(chars[l])):
-                    transform(l, c, chars, out) 
+            for achar in range(len(chars)):
+                for c in range(len(chars[achar])):
+                    transform(achar, c, chars, out)
 
             print_chars(out)
 
+
 if __name__ == "__main__":
     main()
-
